18f3fa736960e96b9af41c332a626737c5166934
Jaroslav Siroic <jaroslav.siroic@gmail.com>
2021-10-10 13:37:03 +0200

    polish lang set for cms ui

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,16 +1,19 @@
-import CMS from 'netlify-cms-app'
-import uploadcare from 'netlify-cms-media-library-uploadcare'
-import cloudinary from 'netlify-cms-media-library-cloudinary'
+import CMS from "netlify-cms-app";
+import uploadcare from "netlify-cms-media-library-uploadcare";
+import cloudinary from "netlify-cms-media-library-cloudinary";
+import { pl } from "netlify-cms-locales";
 
-import AboutPagePreview from './preview-templates/AboutPagePreview'
-import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ContactPagePreview from './preview-templates/ContactPagePreview'
-import IndexPagePreview from './preview-templates/IndexPagePreview'
+import AboutPagePreview from "./preview-templates/AboutPagePreview";
+import BlogPostPreview from "./preview-templates/BlogPostPreview";
+import ContactPagePreview from "./preview-templates/ContactPagePreview";
+import IndexPagePreview from "./preview-templates/IndexPagePreview";
 
-CMS.registerMediaLibrary(uploadcare)
-CMS.registerMediaLibrary(cloudinary)
+CMS.registerLocale("pl", pl);
 
-CMS.registerPreviewTemplate('index', IndexPagePreview)
-CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('contact', ContactPagePreview)
-CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerMediaLibrary(uploadcare);
+CMS.registerMediaLibrary(cloudinary);
+
+CMS.registerPreviewTemplate("index", IndexPagePreview);
+CMS.registerPreviewTemplate("about", AboutPagePreview);
+CMS.registerPreviewTemplate("contact", ContactPagePreview);
+CMS.registerPreviewTemplate("blog", BlogPostPreview);
