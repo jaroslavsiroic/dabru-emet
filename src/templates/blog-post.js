db5bc79f7a20fb4f135392db0bde9eaba435b72a
Jaroslav Siroic <jaroslav.siroic@10clouds.com>
2022-07-16 22:43:51 +0300

    added remarkbox react lib

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,57 +1,15 @@
-import { Comments, FacebookProvider } from "react-facebook";
 import Content, { HTMLContent } from "../components/Content";
 import { Link, Trans } from "gatsby-plugin-react-i18next";
-import React, { useEffect } from "react";
 
 import { Helmet } from "react-helmet";
 import Layout from "../components/Layout";
 import PropTypes from "prop-types";
+import React from "react";
+import Remarkbox from "react-remarkbox";
 import { graphql } from "gatsby";
 import { kebabCase } from "lodash";
 
 const isBrowser = () => typeof window !== "undefined";
-
-const remarkBox = `<!-- Remarkbox - Your readers want to communicate with you -->
-<div id="remarkbox-div">
-  <noscript>
-    <iframe id=remarkbox-iframe src="https://my.remarkbox.com/embed?nojs=true" style="height:600px;width:100%;border:none!important" tabindex=0></iframe>
-  </noscript>
-</div>
-<script src="https://my.remarkbox.com/static/js/iframe-resizer/iframeResizer.min.js"></script>
-<script>
-  var rb_owner_key = "a27913be-8cec-11ec-a3d0-b1c8c4ae987e";
-  var thread_uri = window.location.href;
-  var thread_title = window.document.title;
-  var thread_fragment = window.location.hash;
-
-  // rb owner was here.
-  var rb_src = "https://my.remarkbox.com/embed" + 
-      "?rb_owner_key=" + rb_owner_key +
-      "&thread_title=" + encodeURI(thread_title) +
-      "&thread_uri=" + encodeURIComponent(thread_uri) + 
-      thread_fragment;
-
-  function create_remarkbox_iframe() {
-    var ifrm = document.createElement("iframe");
-    ifrm.setAttribute("id", "remarkbox-iframe");
-    ifrm.setAttribute("scrolling", "no");
-    ifrm.setAttribute("src", rb_src);
-    ifrm.setAttribute("frameborder", "0");
-    ifrm.setAttribute("tabindex", "0");
-    ifrm.setAttribute("title", "Remarkbox");
-    ifrm.style.width = "100%";
-    document.getElementById("remarkbox-div").appendChild(ifrm);
-  }
-  create_remarkbox_iframe();
-  iFrameResize(
-    {
-      checkOrigin: ["https://my.remarkbox.com"],
-      inPageLinks: true,
-      initCallback: function(e) {e.iFrameResizer.moveToAnchor(thread_fragment)}
-    },
-    document.getElementById("remarkbox-iframe")
-  );
-</script>`;
 
 export const BlogPostTemplate = ({
   content,
@@ -63,12 +21,8 @@ export const BlogPostTemplate = ({
   helmet,
 }) => {
   const PostContent = contentComponent || Content;
-  useEffect(() => {
-    setTimeout(() => {
-      document.getElementById("comment-section").innerHTML = remarkBox;
-    }, 1000);
-    console.log("Effect took place");
-  }, []);
+  const url = isBrowser() && window.location.href;
+
   return (
     <section className="section">
       {helmet || ""}
@@ -95,9 +49,14 @@ export const BlogPostTemplate = ({
                 </ul>
               </div>
             ) : null}
-            {/* <FacebookProvider appId="297707888605205">
-              <Comments style={{ width: "100%" }} href={isBrowser() && window.location.href} />
-            </FacebookProvider> */}
+            <Remarkbox
+              style={{
+                width: "100%",
+              }}
+              remarkboxKey={"a27913be-8cec-11ec-a3d0-b1c8c4ae987e"}
+              threadTitle={title}
+              threadUri={url}
+            />
             <div id="comment-section" />
           </div>
         </div>
